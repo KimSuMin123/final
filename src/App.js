216afb4e8d9f889cbf3c1fb97f2cf951a34121b0
Kimsumin123 <tntn211@naver.com>
2023-06-15 13:25:21 +0900

    readme 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [colorCode, setColorCode] = useState("Purple");
+
+  const changeText = (e) => {
+    setColorCode(e.target.value);
+  }
+
+  const applyColor = () => {
+    document.documentElement.style.setProperty(
+      '--background-color', 
+      colorCode
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: "50px 50px"}}>
+      <label>BackgroundColor : {colorCode} </label>
+      <input value={colorCode} onChange={changeText}/>
+      <button onClick={applyColor}>적용</button>
     </div>
   );
 }
